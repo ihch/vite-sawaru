8a7f5a4d7306ed10ced4ff0be9a1c70eff466b41
ihch <ryubi.102395@gmail.com>
2020-08-30 03:09:19 +0900

    feat: props

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,32 @@ div
   img(alt="Vue logo" src="./assets/logo.png")
   HelloWorld(msg="Hello Vue 3.0 + Vite")
   p {{ hoge }}
+  input(v-model="hoge")
 </template>
 
-<script lang='ts'>
-import HelloWorld from './components/HelloWorld.vue'
-import { hoge } from './utils/hoge';
+<script lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+import { hoge } from "./utils/hoge";
 
 export default {
-  name: 'App',
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  props: {
+    msg: {
+      type: String,
+    },
+  },
   data() {
     return {
       hoge: hoge(),
     };
   },
-  components: {
-    HelloWorld
+  methods: {
+    poen() {
+      console.log("hoge");
+    },
   },
-}
+};
 </script>
