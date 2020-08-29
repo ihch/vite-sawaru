b059c61547b6b0b1afb12fc4655360578a4e60b4
ihch <ryubi.102395@gmail.com>
2020-08-29 23:37:18 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+div
+  img(alt="Vue logo" src="./assets/logo.png")
+  HelloWorld(msg="Hello Vue 3.0 + Vite")
+  p {{ hoge }}
+</template>
+
+<script lang='ts'>
+import HelloWorld from './components/HelloWorld.vue'
+import { hoge } from './utils/hoge';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      hoge: hoge(),
+    };
+  },
+  components: {
+    HelloWorld
+  },
+}
+</script>
